22247efca05e6e6609730072d4d54bb69017118a
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2022-11-05 00:46:14 +0700

    Add unit list

---
+++ Assets/Logic/Level/Map/MapHolder.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Logic.Level.Map
+{
+    public class MapHolder : MonoBehaviour
+    {
+        [SerializeField] private List<SpawnPlace> _spawnPlaces;
+        
+        public List<SpawnPlace> SpawnPlaces => _spawnPlaces;
+    }
+
+    [Serializable]
+    public struct SpawnPlace
+    {
+        public List<Transform> SpawnPoints;
+        [Range(0, 3)] public int CommandIndex;
+    }
+}
--- Assets/Logic/Units/UnitList.cs
+++ Assets/Logic/Units/UnitList.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Logic.Units
 {
     [CreateAssetMenu(menuName = "Create UnitList", fileName = "UnitList", order = 0)]
     public class UnitList : ScriptableObject
     {
+        [SerializeField] private SerializedDictionary<UnitType, Unit> _units;
 
+        public Unit GetUnitByType(UnitType type) => _units[type];
     }
 }
