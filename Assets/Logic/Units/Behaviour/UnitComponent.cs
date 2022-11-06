e8d158824d4a4dc148bb2042341f29a201e3e7fc
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2022-11-06 23:42:43 +0700

    Add unit components

---
+++ Assets/Logic/Units/Behaviour/UnitComponent.cs
@@ -0,0 +1,7 @@
+namespace Logic.Units.Behaviour
+{
+    public struct UnitComponent
+    {
+        public Unit Unit;
+    }
+}
--- Assets/Logic/Units/Spawn/UnitSpawnSystem.cs
+++ Assets/Logic/Units/Spawn/UnitSpawnSystem.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Leopotam.EcsLite;
 using Leopotam.EcsLite.Di;
 using Logic.Level.Initialization;
+using Logic.Units.Behaviour;
 using UnityEngine;
 
 namespace Logic.Units.Spawn
@@ -10,6 +11,8 @@ namespace Logic.Units.Spawn
     {
         private readonly EcsWorldInject _world = default;
 
+        private EcsPool<UnitComponent> _unitComponents;
+        private EcsPool<MovementComponent> _movementComponents;
         private EcsPool<MapInformationComponent> _mapInformationComponents;
         private EcsPool<SpawnRequestQueueComponent> _requestQueueComponents;
         private EcsFilter _mapInformationComponentFilter;
@@ -17,6 +20,8 @@ namespace Logic.Units.Spawn
         
         public void Init(IEcsSystems systems)
         {
+            _unitComponents = _world.Value.GetPool<UnitComponent>();
+            _movementComponents = _world.Value.GetPool<MovementComponent>();
             _mapInformationComponents = _world.Value.GetPool<MapInformationComponent>();
             _requestQueueComponents = _world.Value.GetPool<SpawnRequestQueueComponent>();
             _mapInformationComponentFilter = _world.Value.Filter<MapInformationComponent>().End();
@@ -40,6 +45,11 @@ namespace Logic.Units.Spawn
                         var spawnPlace = mapInformationComponent.Map.SpawnPlaces.First(place => place.Number == teamId);
                         var position = spawnPlace.SpawnPoints[Random.Range(0, spawnPlace.SpawnPoints.Count)].position;
                         var unit = Object.Instantiate(unitPrefab, position, Quaternion.identity, mapInformationComponent.Map.transform);
+                        var unitEntity = _world.Value.NewEntity();
+                        ref var unitComponent = ref _unitComponents.Add(unitEntity);
+                        unitComponent.Unit = unit;
+                        ref var movementComponent = ref _movementComponents.Add(unitEntity);
+                        mapEntity.
                     }
                 }
             }
