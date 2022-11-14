21d59a85e183c099f71546df65d9250896c8b982
Vladislav Kryuchkovsky <evakru2009@gmail.com>
2022-11-14 14:47:47 +0700

    Add unit render data list

---
+++ Assets/Logic/Units/Render/UnitRenderDataList.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Logic.Units.Render
+{
+    [CreateAssetMenu(menuName = "Create UnitRenderDataList", fileName = "UnitRenderDataList", order = 0)]
+    public class UnitRenderDataList : ScriptableObject
+    {
+        
+    }
+}
